f4b8eb45cdf74f548b23390db44720d9d830a28f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-05-24 11:10:10 -0400

    run tests on GitHub Actions

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# skip-vs-required-tag
+# skip-vs-required-tag [![ci](https://github.com/bahmutov/skip-vs-required-tag/actions/workflows/ci.yml/badge.svg?branch=main)](https://github.com/bahmutov/skip-vs-required-tag/actions/workflows/ci.yml)
 
 > Example using cy-grep required tag instead of skipping tests
--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,13 @@ module.exports = defineConfig({
     fixturesFolder: false,
     setupNodeEvents(on, config) {
       require('@bahmutov/cy-grep/src/plugin')(config)
+      // bahmutov/cypress-json-results
+      require('cypress-json-results')({
+        on,
+        filename: false, // do not write JSON file
+        githubActionsSummary: 'test',
+      })
+
       // IMPORTANT: return the config object
       return config
     },
